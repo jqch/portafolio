7f0909fd49316e6b6bbd8a0baae2a2cb5ba7bf8f
Jhonny <jhonnyq17@gmail.com>
2018-02-07 15:11:11 -0400

    Primera version

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -5,22 +5,59 @@ import { Http } from '@angular/http';
 export class ProductosService {
 
   productos: any[] = [];
+  productos_filtrado: any[] = [];
   cargando: boolean;
 
   constructor(public http: Http) {
     this.cargar_productos();
   }
 
+  public buscar_producto(termino: string) {
+
+    if (this.productos.length === 0) {
+      this.cargar_productos().then(() => {
+        // termino la carga
+        this.filtrar_productos(termino);
+      });
+    } else {
+      this.filtrar_productos(termino);
+    }
+  }
+
+  private filtrar_productos(termino: string) {
+    this.productos_filtrado = [];
+
+    termino = termino.toLowerCase();
+
+    this.productos.forEach(prod => {
+    
+      if (prod.categoria.indexOf(termino) >= 0 || prod.titulo.toLowerCase().indexOf(termino) >= 0) {
+        this.productos_filtrado.push(prod);
+      }
+      
+    });
+  }
+
+  public cargar_producto(cod: string) {
+    return this.http.get(`https://paginaweb-f32a7.firebaseio.com/productos/${cod}.json`);
+  }
+
   public cargar_productos() {
 
     this.cargando = true;
 
-    this.http.get('https://paginaweb-f32a7.firebaseio.com/productos_idx.json')
-      .subscribe(res => {
-        //console.log(res);
-        this.cargando = false;
-        this.productos = res.json();
-      });
+    let promesa = new Promise((resolve, reject) => {
+      this.http.get('https://paginaweb-f32a7.firebaseio.com/productos_idx.json')
+        .subscribe(res => {
+          //console.log(res);
+          this.cargando = false;
+          this.productos = res.json();
+
+          resolve();
+        });
+    });
+
+    return promesa;
   }
 
 }
